18b4b67afc3cd67e5e98c482e8b036bd919d6965
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2021-03-26 05:29:54 +0100

    added ktor client dependency

--- buildSrc/src/main/kotlin/sk.bsmk.experiments.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/sk.bsmk.experiments.kotlin-common-conventions.gradle.kts
@@ -9,13 +9,14 @@ repositories {
 dependencies {
     constraints {
         implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+        implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
         testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
     }
 
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation(platform("io.ktor:ktor-bom:1.5.2"))
+
     testImplementation("io.kotest:kotest-runner-junit5")
 
 }
--- pipeline-runner/build.gradle.kts
+++ pipeline-runner/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
 
 dependencies {
     implementation("org.apache.logging.log4j:log4j-slf4j-impl")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.microutils:kotlin-logging-jvm")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("io.ktor:ktor-client")
+    implementation("io.ktor:ktor-client-cio")
+
     implementation(project(":pipeline-api"))
 }
 
